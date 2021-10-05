20df1244a9e0f5b765be93a5ff654d09b2a6376b
EDG5000 <hc3d.edg5000@gmail.com>
2021-10-05 16:18:34 +0200

    Simplified data reporting format to ease parsing by plotter application

--- hc3d-tm/src/business_logic/data_reporter.c
+++ hc3d-tm/src/business_logic/data_reporter.c
@@ -19,27 +19,35 @@
 uint16_t data_reporter_frame = 0;
 
 void data_reporter_tick(uint16_t time){
-	str("[%u]\t(%u)", time, data_reporter_frame);
+	//str("[%u]\t(%u)", time, data_reporter_frame);
 
+	// TODO disabled full diagnostic data format to reduce data use and simplify parsing
+
+	/*
 	// Print raw temperatures
 	for(uint8_t sensor_index = 0; sensor_index < HC3D_CONFIG_TEMP_SENSOR_COUNT; sensor_index++){
 		fip raw_temp;
 		util_fip(data_reader_last_temperatures[HC3D_CONFIG_TEMP_BUF_SIZE-1][sensor_index], &raw_temp);
 		str("\trt%u:%u.%u", sensor_index, raw_temp.left, raw_temp.right);
 	}
+	*/
+
 
     // Print valid temperatures and update times
     for(uint8_t sensor_index = 0; sensor_index < HC3D_CONFIG_TEMP_SENSOR_COUNT; sensor_index++){
     	fip valid_temp;
     	util_fip(temp_validator_sensor_last_valid_temperature[sensor_index], &valid_temp);
-    	uint16_t update_time = temp_validator_sensor_last_update_time[sensor_index];
-        str("\tvt%u:%u.%u\tut%u:%u", sensor_index, valid_temp.left, valid_temp.right, sensor_index, update_time);
+    	//uint16_t update_time = temp_validator_sensor_last_update_time[sensor_index];
+        //str("\tvt%u:%u.%u\tut%u:%u", sensor_index, valid_temp.left, valid_temp.right, sensor_index, update_time);
+    	str("\t%u.%u", valid_temp.left, valid_temp.right);
     }
 
+    /*
     // Print PI controller state, pump controller setpoint, tach reading
     uint16_t pump_i = (uint16_t) pump_controller_state.i;
     uint16_t driver_tach_val = driver_tach_get();
     str("\tpwm:%u\tpci:%u\ttac:%u", driver_pwm_value, pump_i, driver_tach_val);
+	*/
 
     str("\n");
     data_reporter_frame++;
