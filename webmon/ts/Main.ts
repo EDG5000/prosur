42f621b33880e59fc9bfbc2fe543f81d3d1e55cf
EDG5000 <hc3d.edg5000@gmail.com>
2021-11-01 09:19:06 +0100

    Removed weird code

--- webmon/ts/Main.ts
+++ webmon/ts/Main.ts
@@ -15,8 +15,9 @@ let init = function(){
     // Get DOM nodes and canvas context
     canvas = document.getElementsByTagName("canvas")[0];
     ctx = canvas.getContext("2d");
-    ctx.translate(0.5, 0.5); // Allegedly "fixes" "blurryness" 
-    ctx.lineWidth = 2;
+    // Uncommented, there is suspicion is causes regression with the positioning, more than half a pixel
+    //ctx.translate(0.5, 0.5); // Allegedly "fixes" "blurryness" 
+    //ctx.lineWidth = 2;
     sessionListContainer = document.getElementById("session-list");
     scroller = document.getElementById("scroller");
     scrollerInner = document.getElementById("scroller-inner");
