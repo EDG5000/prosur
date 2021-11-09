d0a3d29262c6652b842861a206c3b9e11fc340a8
EDG5000 <hc3d.edg5000@gmail.com>
2021-11-09 18:07:45 +0100

    Git update

--- prosurd/src/main.cpp
+++ prosurd/src/main.cpp
@@ -11,13 +11,23 @@ int main() {
 	bool was_printing = false;
 	thread t{[=]{
 			while(true){
-				this_thread::sleep_for(1000ms);
+				// TODO Update chamber temperatures
+				tmclient::update();
+
+
 				rrfclient::update();
 				if(rrfclient::get_is_printing() && !was_printing){
 					// Transition to printing
+					// Insert job, get ID back, store ID globally
 				}else if(!rrfclient::get_is_printing() && was_printing){
 					// Transition to not printing
+					// Set JOB id to NULL
+				}else if(rrfclient::get_is_printing()){
+					// Printing, no transition
 				}
+
+				// Store properties of rrclient_om and tmclient_om in frames table
+
 				cout << "Joinable std::thread" << '\n';
 			}
 	}};
